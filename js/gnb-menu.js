9c9f077af8748d4c881f7c4c7e05c194a7ecc33a
barem21 <barem210@gmail.com>
2024-11-15 18:11:09 +0900

    [wanted clone project] 스크립트 작업 추가

--- js/gnb-menu.js
+++ js/gnb-menu.js
@@ -3,20 +3,33 @@ window.addEventListener("load", () => {
   const liList = document.querySelectorAll(".header-nav li ");
 
   function showFocus(_number) {
-    console.log("전달된 값 ", _number);
     liList.forEach((item, index, c) => {
-      if (_number === index) {
-        console.log("빨간색", index);
+      if (_number !== index) {
+        //console.log("빨간색", index);
+        item.classList.add("nonActive");
       } else {
-        console.log("검은색", index);
+        //console.log("검은색", index);
+        item.classList.remove("nonActive");
       }
     });
   }
 
+  function outFocus() {
+    liList.forEach((item, index, c) => {
+      item.classList.remove("nonActive");
+    });
+  }
+
   liList.forEach((item, index, c) => {
+    //마우스 오버
     item.addEventListener("mouseenter", () => {
       // 선택된 번호를 전달해서 꾸미기
       showFocus(index);
+    });
+
+    //마우스 아웃
+    item.addEventListener("mouseleave", () => {
+      outFocus();
     });
   });
 });
